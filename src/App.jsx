df0396941ea9933d15db3199680460bc8d72d07b
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-16 11:10:51 -0700

    lesson_1_5 finished. Custom hook created. Saving and retriving data from localstorage. Fragment implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { list } from "./data.jsx";
 import { TodoList } from "./TodoList.jsx";
 import { AddTodoForm } from "./AddTodoForm.jsx";
 
-const App = () => {
+const userSemiPersistentState = () => {
 	const [todoList, setTodoList] = useState(
-		JSON.parse(localStorage.getItem("savedTodoList"))
+		JSON.parse(localStorage.getItem("savedTodoList")) ?? list
 	);
 
 	const toLocalStorage = () => {
@@ -14,17 +14,22 @@ const App = () => {
 	};
 
 	useEffect(toLocalStorage, [todoList]);
+	return [todoList, setTodoList];
+};
+
+const App = () => {
+	const [todoList, setTodoList] = userSemiPersistentState();
 
 	const addTodo = (newTodo) => {
 		setTodoList([...todoList, newTodo]);
 	};
 
 	return (
-		<div className="main-wrapper">
+		<>
 			<h1>Todo List</h1>
 			<AddTodoForm onAddTodo={addTodo} />
 			<TodoList todoList={todoList} />
-		</div>
+		</>
 	);
 };
 export { App };
