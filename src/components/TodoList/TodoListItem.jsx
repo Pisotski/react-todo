e326f3d39e25046cba6a4a17c0ff5e9dd4ac9f4a
Pisotski <vlad.pisotski@gmail.com>
2024-05-20 18:30:34 -0700

    added svg

--- src/components/TodoList/TodoListItem.jsx
+++ src/components/TodoList/TodoListItem.jsx
@@ -1,10 +1,12 @@
+import Remove from "../../assets/remove-minus-circle-svgrepo-com.svg?react";
+
 const TodoListItem = ({ item: { id, title }, onRemoveTodo }) => {
 	return (
 		<li>
 			<input type="checkbox" id={title} name={title} value={title} />
 			<label htmlFor={title}>{title}</label>
 			<button type="button" onClick={() => onRemoveTodo(id)}>
-				Remove
+				<Remove height="25px" width="25px" />
 			</button>
 		</li>
 	);
